8c3cacab46ad5ca08690cc31f2d3b6bbf6067adf
youmekko <youmekko.o@gmail.com>
2018-10-21 23:57:37 +0900

    프로젝트 scaffolding

---
+++ 1-vanilla/js/app.js
@@ -0,0 +1,7 @@
+import MainController from './controller/MainController.js'
+
+
+//DOM이 다 완성이 됐을 때, MainController 호출
+document.addEventListener('DOMContentLoaded', ()=>{
+    MainController.init();
+})
