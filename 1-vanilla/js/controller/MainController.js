5865f2338d049cf1beb9ada0bef462257aa66dde
youmekko <youmekko.o@gmail.com>
2018-12-05 11:20:35 +0900

    ResultView 추가하고 검색결과 없을시 메세지 표시하기까지 구현

--- 1-vanilla/js/controller/MainController.js
+++ 1-vanilla/js/controller/MainController.js
@@ -1,4 +1,7 @@
 import FormView from '../views/FormView.js'
+import ResultView from '../views/ResultView.js'
+
+import SearchModel from '../models/SearchModel.js'
 
 const tag ='[MainController]'
 
@@ -10,13 +13,30 @@ export default {
         FormView.setup(document.querySelector('form'))
             .on('@submit', e => this.onSubmit(e.detail.input))
             .on('@reset', e => this.onResetForm())
+
+
+        ResultView.setup(document.querySelector('#search-result'))
+    },
+
+
+    search(query){
+        console.log(tag, 'search()', query)
+        SearchModel.list(query).then(data=>{
+            this.onSearchResult(data)
+        })
+
     },
 
     onSubmit(input){
         console.log(tag,'onSubmit()', input)
+        this.search(input)
     },
 
     onResetForm(){
         console.log(tag, 'onResetForm()');
+    },
+
+    onSearchResult(data){
+        ResultView.render(data)
     }
 }
